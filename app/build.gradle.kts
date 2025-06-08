c5a5971578d9b9277d950d3d38863028db8979dc
Pablo Bozzolo <boctulus@gmail.com>
2025-06-09 01:52:19 +0800

    Automatic update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,6 +107,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.activity.compose)
     
