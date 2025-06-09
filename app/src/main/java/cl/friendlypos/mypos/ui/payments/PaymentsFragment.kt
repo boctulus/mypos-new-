5e9ea0307bd25f68a2acda8ce1b0c43b9b0cbc64
Pablo Bozzolo <boctulus@gmail.com>
2025-06-09 19:26:12 +0800

    PAGOS OK

--- app/src/main/java/cl/friendlypos/mypos/ui/payments/PaymentsFragment.kt
+++ app/src/main/java/cl/friendlypos/mypos/ui/payments/PaymentsFragment.kt
@@ -2,32 +2,19 @@ package cl.friendlypos.mypos.ui.payments
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import cl.friendlypos.mypos.databinding.FragmentPaymentsBinding
+import cl.friendlypos.mypos.compose.screen.PaymentsScreen
 
 class PaymentsFragment : Fragment() {
-
-    private var _binding: FragmentPaymentsBinding? = null
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentPaymentsBinding.inflate(inflater, container, false)
-        return binding.root
+    ) = ComposeView(requireContext()).apply {
+        setContent {
+            PaymentsScreen()
+        }
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.textPayments.text = "Payments Fragment"
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}   
+}
