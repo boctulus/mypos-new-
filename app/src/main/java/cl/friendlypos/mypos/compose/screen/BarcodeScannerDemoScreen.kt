1be8a8cd1ca47fe53725ff40130d772e2c3ba889
Pablo Bozzolo <boctulus@gmail.com>
2025-06-09 14:42:25 +0800

    Vista dummy de Scanner OK

--- app/src/main/java/cl/friendlypos/mypos/compose/screen/BarcodeScannerDemoScreen.kt
+++ app/src/main/java/cl/friendlypos/mypos/compose/screen/BarcodeScannerDemoScreen.kt
@@ -35,12 +35,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-
 import cl.friendlypos.mypos.compose.viewmodel.BarcodeScannerViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BarcodeScannerScreen(
+fun BarcodeScannerDemoScreen (
     onBackPressed: () -> Unit,
     viewModel: BarcodeScannerViewModel = viewModel()
 ) {
